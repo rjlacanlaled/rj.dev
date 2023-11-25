c03b67ffe047598db1a945c98095d2f308eae5b3
rjlacanlaled <rjlacanlaled@gmail.com>
2023-11-25 13:38:46 +0800

    some stuff

--- apps/web/src/pages/home.rs
+++ apps/web/src/pages/home.rs
@@ -1,20 +1,20 @@
-use actix_web::{ HttpRequest, Result as ActixResult, get };
-use maud::{ Markup, html };
+use actix_web::{get, HttpRequest, Result as ActixResult};
+use maud::{html, Markup};
 
 use crate::pages::base::index;
-
 
 #[get("/")]
 pub async fn page(req: HttpRequest) -> ActixResult<Markup> {
     let title: &str = "Home";
     let desc = "Rj Dev Home Page";
 
-    let content =
-        html! {
-            div  {
-                p class="text-red-500" { "Hello, Worlds!" }
-             }
-        };
+    let content = html! {
+        div class="container bg-red-300 w-full min-h-screen" {
+            div class="container  h-full w-full p-10 bg-blue-300" {
+                p { "Hello World" }
+            }
+         }
+    };
 
     Ok(index(req, content, title, desc).await)
 }
